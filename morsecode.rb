787d6baececa134650a1a0ddf662ebf9c0e01d96
BHASKAR RANJAN BORA <bhaskarranjan44@gmail.com>
2023-01-25 17:06:45 +0530

    Create morsecode.rb

---
+++ morsecode.rb
@@ -0,0 +1,41 @@
+MORSE_CODE_DICT = { 'A' => '.-', 'B' => '-...', 
+                    'C' => '-.-.', 'D' => '-..', 'E' => '.', 
+                    'F' => '..-.', 'G' => '--.', 'H' => '....', 
+                    'I' => '..', 'J' => '.---', 'K' => '-.-', 
+                    'L' => '.-..', 'M' => '--', 'N' => '-.', 
+                    'O' => '---', 'P' => '.--.', 'Q' => '--.-', 
+                    'R' => '.-.', 'S' => '...', 'T' => '-', 
+                    'U' => '..-', 'V' => '...-', 'W' => '.--', 
+                    'X' => '-..-', 'Y' => '-.--', 'Z' => '--..', 
+                    '1' => '.----', '2' => '..---', '3' => '...--', 
+                    '4' => '....-', '5' => '.....', '6' => '-....', 
+                    '7' => '--...', '8' => '---..', '9' => '----.', 
+                    '0' => '-----', ', ' => '--..--', '.' => '.-.-.-', 
+                    '?' => '..--..', '/' => '-..-.', '-' => '-....-', 
+                    '(' => '-.--.', ')' => '-.--.-'} 
+
+def decode_char(morse_code)
+    MORSE_CODE_DICT.each do |char, code|
+        if code == morse_code
+            return char.upcase
+        end
+    end
+end
+
+def decode_word(morse_code)
+    words = morse_code.split(" ")
+    decoded_word = ""
+    words.each do |word|
+        decoded_word += decode_char(word)
+    end
+    return decoded_word
+end
+
+def decode_message(morse_code)
+    words = morse_code.split("   ")
+    decoded_message = ""
+    words.each do |word|
+        decoded_message += decode_word(word) + " "
+    end
+    return decoded_message
+end
